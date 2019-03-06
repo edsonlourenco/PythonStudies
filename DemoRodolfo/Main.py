85c8831da1f8e69149801afea6339e16d79e87dc
Edson Loure <edson.lourenco@outlook.com>
2019-03-06 09:15:08 -0300

    Construção do FileWatcher

---
+++ DemoRodolfo/Main.py
@@ -0,0 +1,5 @@
+# É um modulo em Python
+from Pacote.ModDemo import Demo
+
+demo = Demo()
+demo.contar(1000)
---
+++ DemoRodolfo/Pacote/ModDemo.py
@@ -0,0 +1,8 @@
+class Demo:
+    def __init__(self):
+        pass
+
+    def contar(self, total):
+        numeros = range(total)
+        for numero in numeros:
+            print(numero)
